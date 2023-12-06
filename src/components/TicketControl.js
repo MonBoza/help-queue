ba70af37e6c7ddc571ad8255aafd1ac76e461ea0
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-12-06 14:42:12 -0600

    add UUID

---
+++ src/components/TicketControl.js
@@ -0,0 +1,49 @@
+import React from "react";
+import NewTicketForm from "./NewTicketForm";
+import TicketList from "./TicketList";
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      formVisibleOnPage: false, 
+      mainTicketList: [] //add this prop and pass down as prop to TicketList. empty so don't start with fake tickets
+    };
+  }
+  handleClick = () => {
+    this.setState(prevState =>({formVisibleOnPage: !prevState.formVisibleOnPage}));
+  }
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({
+      mainTicketList: newMainTicketList, 
+      formVisibleOnPage: false
+    });
+  } //false so user sees queue again, not form
+  render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />; //passing handle() down to NewtickForm as prop called onNewTicketCreation
+      buttonText = "Return to Ticket List";
+    } else {
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList}/>; //passing down prop to TicketList child
+      buttonText = "Add Ticket";
+      
+    }
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TicketControl;
+
+
+
+// deleting the mock ticket list broke all my code ..
